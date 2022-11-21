300178226d14cc6f2de999219d61588c47037939
Kazungu Derrick <kazunguderriq@gmail.com>
2022-11-21 15:07:56 +0300

    done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,8 +1,14 @@
 class CheesesController < ApplicationController
-
   def index
     cheeses = Cheese.all
     render json: cheeses
   end
 
+  def show
+    # find cheese using id from the url
+    # send jsom response using that cheese object
+    cheese = Cheese.find_by(id: params[:id])
+    render json: cheese
+
+  end
 end
